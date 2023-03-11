338bf757cf03dfad3dfb65fe00a4b2f6a88a625f
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-03-11 16:33:49 -0500

    Added the recipe model.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Recipe(models.Model):
+    recipe =models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"Recipe: {self.recipe}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Recipe
+import openai
 
-# Create your views here.
+
+
