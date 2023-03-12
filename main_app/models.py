0aac206548130347d3f2a719e01b5f9e7bdb0dab
victor <victor@victors-MacBook-Air.local>
2023-03-12 17:54:14 -0400

    got the nav bar working

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+ 
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    ingredient = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'recipe_id: self.id'})
