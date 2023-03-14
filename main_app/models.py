f21040471c35888f9c34616939a1e349c20a44c8
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-03-13 22:22:02 -0400

    Added some code to resolve the issue with making an API call. It still doesn't work.

--- main_app/models.py
+++ main_app/models.py
@@ -3,10 +3,19 @@ from django.urls import reverse
 
 
 class Recipe(models.Model):
+    ingredients = models.TextField(max_length=500)
     recipe = models.TextField(max_length=2500)
 
     def __str__(self):
         return f"Recipe: {self.recipe}"
 
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'recipe_id: self.id'})
+
+# class Ingredients(models.Model):
+#     ingredients = models.TextField(max_length=500)
+#     recipe = models.OneToOneField(Recipe, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f"Ingredients: {self.ingredients}"
+#
+#     def get_absolute_url(self):
+#         return reverse('detail', kwargs={'recipe_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Recipe
 import openai
 
 
-def get_recipe(request):
-    prompt = "user input"
+def get_recipe(user_prompt):
+    prompt = f"Give me a recipe using the following ingredients: {user_prompt}"
 
     openai.api_key = open("key.txt", "r").read().strip('\n')
     completion = openai.ChatCompletion.create(
@@ -18,13 +18,13 @@ def get_recipe(request):
 
     ChatGPT_recipe = completion["choices"][0]["message"]["content"]
 
-from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, DeleteView
-from .models import Recipe
+    return ChatGPT_recipe
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
 
 def about(request):
     return render(request, 'about.html')
@@ -34,15 +34,17 @@ def recipes_index(request, recipe_id):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/index.html', { 'recipes': recipes})
 
+
 def recipes_detail(request,recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     return render(request, 'recipes/detail.html', {'recipe': recipe})
 
+
 class RecipeCreate(CreateView):
     model = Recipe
-    fields = ['name','ingredient']
+    fields = ["ingredients"]
 
-    
+
 class RecipeDelete(DeleteView):
     model = Recipe
     success_url = '/recipes/'
