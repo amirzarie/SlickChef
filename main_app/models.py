fda51f5cbefb20358677bf94139756647686f652
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-03-13 20:23:35 -0400

    testing

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 class Recipe(models.Model):
-    recipe =models.TextField(max_length=1000)
+    recipe = models.TextField(max_length=2500)
 
     def __str__(self):
         return f"Recipe: {self.recipe}"
