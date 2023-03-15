99e5b3cc688fe90693f013068dc821122c479973
Ryan Carr <ryan.g.carr@hotmail.com>
2023-03-14 20:04:43 -0400

    add templates and views requirements. Cleaned up existing views doubles and base html

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
-from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView
+
+# Auth
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+# Model
 from .models import Recipe
+
+# API
 import openai
 
 
@@ -18,17 +28,11 @@ def get_recipe(request):
 
     ChatGPT_recipe = completion["choices"][0]["message"]["content"]
 
-from django.shortcuts import render
-from django.views.generic import CreateView, UpdateView, DeleteView
-from .models import Recipe
-
-# Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
-
 
 def recipes_index(request, recipe_id):
     recipes = Recipe.objects.all()
@@ -41,8 +45,7 @@ def recipes_detail(request,recipe_id):
 class RecipeCreate(CreateView):
     model = Recipe
     fields = ['name','ingredient']
-
-    
+   
 class RecipeDelete(DeleteView):
     model = Recipe
     success_url = '/recipes/'
