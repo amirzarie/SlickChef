3584f630da7e1530e20347bc0c613246486665bc
Ryan Carr <ryan.g.carr@hotmail.com>
2023-03-14 20:25:32 -0400

    finish auth, left note in views outlining what needs to be done for auth after testing

--- main_app/views.py
+++ main_app/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+# Need to add LoginRequiredMixin and login_required to all views we want to restrict. Leaving it out for now for ease of testing. - Ryan
+
 # Model
 from .models import Recipe
 
@@ -33,6 +35,21 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def signup(request):
+    error_message = ''
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 def recipes_index(request, recipe_id):
     recipes = Recipe.objects.all()
