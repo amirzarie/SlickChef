2a946447877351d8846cc6efc6c1a542555c100b
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-03-15 19:52:14 -0400

    Created function based view to receive the ingredients from the user and get the recipe.

--- main_app/models.py
+++ main_app/models.py
@@ -9,13 +9,5 @@ class Recipe(models.Model):
     def __str__(self):
         return f"Recipe: {self.recipe}"
 
-
-# class Ingredients(models.Model):
-#     ingredients = models.TextField(max_length=500)
-#     recipe = models.OneToOneField(Recipe, on_delete=models.CASCADE)
-#
-#     def __str__(self):
-#         return f"Ingredients: {self.ingredients}"
-#
-#     def get_absolute_url(self):
-#         return reverse('detail', kwargs={'recipe_id': self.id})
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'recipe_id': self.id})
