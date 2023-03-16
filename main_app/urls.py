051cc0cc76185033daf642ca1836f5d048dcff1f
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-03-15 21:48:39 -0400

    Removed a lot of files that weren't fully developed using the infamous 5 steps. Right now, the web app can receive ingredients from user and generate a recipe.

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,13 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('recipes/', views.recipes_index, name='index'),
-    path('recipes/create/', views.create_recipe, name='create_recipe'),
-    path('recipes/<int:recipe_id>/', views.recipes_detail, name='recipe_detail'),
-    path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipes_delete'),
-    path('recipes/<int:recipe_id>/', views.recipes_detail, name='detail'),
-    path('recipes/<int:pk>/delete/', views.RecipeDelete.as_view(), name='recipes_delete'),
-    
+    path('main_app/get_ingredients', views.get_ingredients, name='get_ingredients'),
+    path('recipes/show_recipe', views.show_recipe, name='show_recipe'),
+
     # Accounts
     path('accounts/signup/', views.signup, name='signup'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
-from .forms import RecipeForm
 
 # Auth
 from django.contrib.auth import login
@@ -17,22 +16,6 @@ from .models import Recipe
 
 # API
 import openai
-
-
-def get_recipe(user_prompt):
-    prompt = f"Give me a recipe using the following ingredients: {user_prompt}"
-
-    openai.api_key = open("key.txt", "r").read().strip('\n')
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages = [{
-            "role": "user", "content": prompt
-        }]
-    )
-
-    ChatGPT_recipe = completion["choices"][0]["message"]["content"]
-
-    return ChatGPT_recipe
 
 
 # Create your views here.
@@ -60,31 +43,15 @@ def signup(request):
     return render(request, 'registration/signup.html', context)
 
 
-def recipes_index(request, recipe_id):
-    recipes = Recipe.objects.all()
-    return render(request, 'recipes/index.html', { 'recipes': recipes})
+def get_ingredients(request):
+    return render(request, 'main_app/get_ingredients.html')
 
 
-def recipes_detail(request, recipe_id):
-    recipe = Recipe.objects.get(id=recipe_id)
-    return render(request, 'recipes/detail.html', {'recipe': recipe})
-
-
-class RecipeCreate(CreateView):
-    model = Recipe
-    fields = ["recipe"]
-
-
-class RecipeDelete(DeleteView):
-    model = Recipe
-    success_url = '/recipes/'
-
-
-def create_recipe(request):
+def show_recipe(request):
     user_prompt = request.POST
     prompt = f"Give me a recipe using the following ingredients: {user_prompt}"
 
-    openai.api_key = open("key.txt", "r").read().strip('\n')
+    openai.api_key = open("main_app/key.txt", "r").read().strip('\n')
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{
@@ -93,10 +60,4 @@ def create_recipe(request):
     )
 
     chatgpt_recipe = completion["choices"][0]["message"]["content"]
-
-    form = RecipeForm(initial={"recipe": chatgpt_recipe})
-    if form.is_valid():
-        new_recipe = form.save(commit=False)
-        new_recipe.save()
-        return redirect('detail', recipe_id=new_recipe.id)
-    return render(request, 'create_recipe.html', {'form': form})
+    return render(request, 'recipes/show_recipe.html', { "recipe": chatgpt_recipe })
